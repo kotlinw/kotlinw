1dc977adf33f6ade0d7663f8000317c728df5bb8
Norbert Sándor <sandor.norbert@erinors.com>
2022-06-13 11:50:57 +0200

    HOMEAUT-110: improve build script

--- kotlinw-immutator-processor/build.gradle.kts
+++ kotlinw-immutator-processor/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 kotlin {
     dependencies {
         implementation(project(":kotlinw-immutator-api"))
-        implementation("com.google.devtools.ksp:symbol-processing-api:1.6.21-1.0.5")
+        implementation("com.google.devtools.ksp:symbol-processing-api:1.6.21-1.0.6")
         implementation("com.squareup:kotlinpoet-ksp:1.11.0")
         implementation(kotlin("stdlib-jdk8"))
         implementation(libs.kotlinx.collections.immutable)
