80199b06ae815bb7c21a3ce43016bb06a9912ade
Norbert Sándor <sandor.norbert@erinors.com>
2022-07-11 17:45:15 +0200

    Upgrade Kotlin to 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,9 @@ buildscript {
 }
 
 plugins {
-    kotlin("multiplatform") version "1.7.0" apply false
-    kotlin("plugin.serialization") version "1.7.0" apply false
-    id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply false
+    kotlin("multiplatform") version "1.7.10" apply false
+    kotlin("plugin.serialization") version "1.7.10" apply false
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
     id("org.jetbrains.compose") version "1.2.0-alpha01-dev741" apply false
 }
 
