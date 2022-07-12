6c620793cb5c05d7b544e35a2c3b34671fdec070
Norbert Sándor <sandor.norbert@erinors.com>
2022-07-12 22:37:22 +0200

    Upgrade Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     kotlin("multiplatform") version "1.7.10" apply false
     kotlin("plugin.serialization") version "1.7.10" apply false
     id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev741" apply false
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev745" apply false
 }
 
 subprojects {
