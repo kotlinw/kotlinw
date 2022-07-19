b833aced6f631b1a32308cf2d4e9991d9975f1c7
Norbert Sándor <sandor.norbert@erinors.com>
2022-07-19 18:39:46 +0200

    upgrade Compose, downgrade to Kotlin 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@ buildscript {
 }
 
 plugins {
-    kotlin("multiplatform") version "1.7.10" apply false
-    kotlin("plugin.serialization") version "1.7.10" apply false
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev745" apply false
+    kotlin("multiplatform") version "1.7.0" apply false
+    kotlin("plugin.serialization") version "1.7.0" apply false
+    id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply false
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev748" apply false
 }
 
 subprojects {
