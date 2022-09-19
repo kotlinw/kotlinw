2d4877a6ff38f286504a636e6338f76c6bc62c0e
Norbert Sándor <sandor.norbert@erinors.com>
2022-09-19 20:46:28 +0200

    + partial jvm 1.8 support

--- kotlinw-statemachine-example-desktop/build.gradle.kts
+++ kotlinw-statemachine-example-desktop/build.gradle.kts
@@ -9,6 +9,14 @@ plugins {
 
 kotlin {
     target {
+        attributes {
+            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+        }
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
     }
 }
 
