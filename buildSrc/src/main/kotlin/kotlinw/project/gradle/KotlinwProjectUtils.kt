dc4695ec377206396b8e01fe38b88c8563ad1e19
Norbert Sándor <sandor.norbert@erinors.com>
2022-10-03 10:49:13 +0200

    fix build

---
+++ buildSrc/src/main/kotlin/kotlinw/project/gradle/KotlinwProjectUtils.kt
@@ -0,0 +1,66 @@
+package kotlinw.project.build.gradle
+
+import kotlinw.project.build.gradle.DevelopmentMode.Production
+import org.gradle.api.Project
+import java.util.*
+
+fun Project.getLocalPropertyValue(key: String): String? =
+    rootProject.file("local.properties").let {
+        if (it.exists()) {
+            Properties().apply { load(it.reader()) }.getProperty(key)
+        } else {
+            null
+        }
+    }
+
+private const val developmentModeOptionName = "kotlinw.build.mode"
+
+enum class DevelopmentMode {
+    Development,
+    Production
+}
+
+// TODO cleanup
+val Project.buildMode: DevelopmentMode
+    get() {
+        val developmentModeSystemPropertyValue = System.getProperty(developmentModeOptionName)
+
+        fun determineBuildMode(value: String?): DevelopmentMode =
+            DevelopmentMode.values().firstOrNull { it.name.toLowerCase() == value?.toLowerCase() }
+                ?: throw IllegalStateException("Invalid build mode: $value")
+
+        return if (developmentModeSystemPropertyValue != null) {
+            // TODO log
+            determineBuildMode(developmentModeSystemPropertyValue)
+        } else {
+            determineBuildMode(getLocalPropertyValue(developmentModeOptionName))
+        }
+    }
+
+data class SigningData(
+    val privateKey: String,
+    val password: String
+)
+
+fun Project.readSigningData(): SigningData {
+    check(buildMode == Production)
+
+    return SigningData(
+        getLocalPropertyValue("signing.privateKey")!!,
+        getLocalPropertyValue("signing.password")!!
+    )
+}
+
+data class OssrhAccountData(
+    val username: String,
+    val password: String
+)
+
+fun Project.readOssrhAccountData(): OssrhAccountData {
+    check(buildMode == Production)
+
+    return OssrhAccountData(
+        getLocalPropertyValue("ossrh.username")!!,
+        getLocalPropertyValue("ossrh.password")!!
+    )
+}
