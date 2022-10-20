a74554b59913a580aa396827961c11fbeb9482b8
Norbert Sándor <sandor.norbert@erinors.com>
2022-10-20 16:58:50 +0200

    .

--- kotlinw-compose/src/commonMain/kotlin/kotlinw/compose/remoting/produceStateByRemoteCall.kt
+++ kotlinw-compose/src/commonMain/kotlin/kotlinw/compose/remoting/produceStateByRemoteCall.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.produceState
 import kotlinw.remoting.api.RemoteService
-import kotlinw.remoting.module.Remoting
-import kotlinw.remoting.module.callRemote
+import kotlinw.remoting.api.Remoting
+import kotlinw.remoting.api.callRemote
 import kotlinw.remoting.api.ApplicationNodeId
 import kotlin.reflect.KSuspendFunction2
 
@@ -18,7 +18,7 @@ data class DataFetchSuccessful<Response>(val response: Response) : DataFetchStat
 
 // TODO util-ba
 @Composable
-@Deprecated(message = "see: kotlinw.remoting.module.Remoting.produceRemoteCallState()")
+@Deprecated(message = "see: kotlinw.remoting.api.Remoting.produceRemoteCallState()")
 inline fun <reified T : RemoteService, reified Request : Any, reified Response : Any> Remoting.produceStateByRemoteCall(
     nodeId: ApplicationNodeId,
     method: KSuspendFunction2<T, Request, Response>,
