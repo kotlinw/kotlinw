1a1c33ee62b0fbf3b6d237de8c8efc2355629d90
Norbert Sándor <sandor.norbert@erinors.com>
2022-12-09 22:03:55 +0100

    .

---
+++ kotlinw-util/src/commonMain/kotlin/kotlinw/util/concurrent/AtomicBoolean.kt
@@ -0,0 +1,15 @@
+package kotlinw.util.concurrent
+
+expect class AtomicBoolean constructor(initialValue: Boolean) {
+    fun get(): Boolean
+
+    fun set(value: Boolean)
+
+    fun compareAndSet(expectedValue: Boolean, newValue: Boolean): Boolean
+
+    fun getAndSet(newValue: Boolean): Boolean
+}
+
+inline var AtomicBoolean.value
+    get() = get()
+    set(value) = set(value)
---
+++ kotlinw-util/src/jvmMain/kotlin/kotlinw/util/concurrent/AtomicBoolean.kt
@@ -0,0 +1,4 @@
+@file:JvmName("ApplicationStateJvm")
+package kotlinw.util.concurrent
+
+actual typealias AtomicBoolean = java.util.concurrent.atomic.AtomicBoolean
