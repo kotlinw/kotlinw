d92cb760230c6c1bb739b54de8bba3b05fa891f6
Norbert Sándor <sandor.norbert@erinors.com>
2022-12-16 09:43:35 +0100

    .

--- kotlinw-compose/src/jvmMain/kotlin/kotlinw/compose/modifier/ext/RecomposeHighlighter.kt
+++ kotlinw-compose/src/jvmMain/kotlin/kotlinw/compose/modifier/ext/RecomposeHighlighter.kt
@@ -1,23 +1,23 @@
 // Source: https://raw.githubusercontent.com/android/snippets/master/compose/recomposehighlighter/src/main/java/com/example/android/compose/recomposehighlighter/RecomposeHighlighter.kt
-// Version: 164f256 on Mar 1 (https://github.com/android/snippets/commit/164f25686ee3ca16b3c07078a3dc357319d23ded)
+// Version: 0655c15 on 2022-12-16 (https://github.com/android/snippets/blob/337e77d592712ddb6f207e157607e43d0c62dfe8/compose/recomposehighlighter/src/main/java/com/example/android/compose/recomposehighlighter/RecomposeHighlighter.kt)
 
 package kotlinw.compose.modifier.ext
 
 /*
- *  Copyright 2022 Google Inc.
+ * Copyright 2022 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-// package com.example.android.compose.recomposehighlighter
 
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
