93ca7767e4ef6672e1de612d8df1e4860ab40ed8
Norbert Sándor <sandor.norbert@erinors.com>
2022-12-18 16:40:30 +0100

    .

--- kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/ResultUtils.kt
+++ kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/ResultUtils.kt
@@ -1,7 +1,9 @@
 package kotlinw.util.stdlib
 
 import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.map
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
@@ -18,3 +20,16 @@ inline fun <V, E> Result<V, E>.ifError(block: (E) -> Unit): Result<V, E> {
 
     return this
 }
+
+fun <V, E> Iterable<Result<V, E>>.combineWithAllErrors(): Result<List<V>, List<E>> =
+    if (any { it is Err<*> }) {
+        Err(filterIsInstance<Err<E>>().map { it.error })
+    } else {
+        Ok(map { (it as Ok<V>).value })
+    }
+
+fun <V, E> combineWithAllErrors(vararg results: Result<V, E>): Result<List<V>, List<E>> =
+    results.asIterable().combineWithAllErrors()
+
+fun <V, E, U> produceIfAllOk(vararg results: Result<V, E>, produce: () -> U): Result<U, List<E>> =
+    results.asIterable().combineWithAllErrors().map { produce() }
--- kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/StaxUtils.kt
+++ kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/StaxUtils.kt
@@ -1,5 +1,8 @@
 package kotlinw.util.stdlib
 
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
 import java.io.Reader
 import javax.xml.namespace.QName
 import javax.xml.stream.XMLEventReader
@@ -9,6 +12,14 @@ import javax.xml.stream.events.StartElement
 fun Reader.createXmlEventReader(xmlInputFactory: XMLInputFactory = XMLInputFactory.newInstance()): XMLEventReader =
     xmlInputFactory.createXMLEventReader(this)
 
-operator fun StartElement.get(attributeNameLocalPart: String): String =
-    getAttributeByName(QName(attributeNameLocalPart))?.value
-        ?: throw IllegalStateException("Required attribute '$attributeNameLocalPart' does not exist.")
+enum class GetAttributeValueError {
+    NotFound
+}
+
+operator fun StartElement.get(attributeSimpleName: String): Result<String, GetAttributeValueError> =
+    getAttributeByName(
+        QName(attributeSimpleName)
+    )
+        ?.value
+        ?.let { Ok(it) }
+        ?: Err(GetAttributeValueError.NotFound)
