254696d2c2d2a6897d16ac264d0c00808cf2a725
Norbert Sándor <sandor.norbert@erinors.com>
2022-12-19 17:53:18 +0100

    .

--- kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/ClassUtils.kt
+++ kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/ClassUtils.kt
@@ -3,3 +3,8 @@ package kotlinw.util.stdlib
 import kotlin.reflect.KClass
 
 actual val KClass<*>.debugName: String get() = qualifiedName ?: "<unknown class>"
+
+fun KClass<*>.noArgsConstructor() = java.declaredConstructors.first { it.parameterCount == 0 }
+
+inline fun <reified T : Any> KClass<T>.newInstance() =
+    noArgsConstructor().also { it.trySetAccessible() }.newInstance() as T
