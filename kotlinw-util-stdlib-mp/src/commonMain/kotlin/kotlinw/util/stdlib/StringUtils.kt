b4e198f0e1c839bb3216af16a5d9e5b25f9eb1de
Norbert Sándor <sandor.norbert@erinors.com>
2023-01-05 16:12:07 +0100

    .

--- kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/StringUtils.kt
+++ kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/StringUtils.kt
@@ -1,3 +1,12 @@
 package kotlinw.util.stdlib
 
 fun String.ifBlankNull() = ifBlank { null }
+
+fun String.splitByNewLines(excludeBlankLines: Boolean = true): List<String> =
+    split("\r\n", "\n").let { lines ->
+        if (excludeBlankLines) {
+            lines.filter { it.isNotBlank() }
+        } else {
+            lines
+        }
+    }
