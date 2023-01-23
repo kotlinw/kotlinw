293cbe991f7d8b378cb8e28875022a3fec7a445f
Norbert Sándor <sandor.norbert@erinors.com>
2023-01-23 10:33:52 +0100

    .

--- kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/OpaqueValue.kt
+++ kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/OpaqueValue.kt
@@ -1,8 +1,6 @@
 package kotlinw.util.stdlib
 
-import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 
-@Serializable
 @JvmInline
-value class OpaqueValue(val value: String)
+value class OpaqueValue(val value: Any)
