2b5fde3fa55769b4e7e7f9aad0e9fc70318a190f
Norbert Sándor <sandor.norbert@erinors.com>
2023-04-12 18:35:58 +0200

    .

--- kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/collection/MapUtils.kt
+++ kotlinw-util-stdlib-mp/src/commonMain/kotlin/kotlinw/util/stdlib/collection/MapUtils.kt
@@ -4,3 +4,6 @@ fun <K, V> Iterable<Map<K, V>>.merge(): Map<K, V> =
     mutableMapOf<K, V>().also { result -> forEach { result.putAll(it) } }
 
 fun <K, V> Map<K, V>.containsKeys(keys: Iterable<K>) = keys.all { containsKey(it) }
+
+@Suppress("UNCHECKED_CAST")
+fun <K, V> Map<K, V?>.filterNotNullValues(): Map<K, V> = filterValues { it != null } as Map<K, V>
