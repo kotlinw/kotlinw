87b003d7001c5b8d4bdb4e1d17ec2195ca552cf7
Norbert Sándor <sandor.norbert@erinors.com>
2023-05-17 14:20:25 +0200

    code cleanup

--- kotlinw-configuration-core/src/commonMain/kotlin/kotlinw/configuration/core/DelegatingFileConfigurationPropertySource.kt
+++ kotlinw-configuration-core/src/commonMain/kotlin/kotlinw/configuration/core/DelegatingFileConfigurationPropertySource.kt
@@ -11,27 +11,44 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
-class DelegatingFileConfigurationPropertySource(
+class DelegatingFileConfigurationPropertySource private constructor(
     private val fileLocation: FileLocation,
     private val delegateFactory: (String) -> EnumerableConfigurationPropertySource,
     override val priority: Priority = Priority.Normal,
-    watcherCoroutineScope: CoroutineScope? = null,
-    eventBus: LocalEventBus? = null,
-    private val watchDelay: Duration = Duration.INFINITE
+    watcherCoroutineScope: CoroutineScope?,
+    eventBus: LocalEventBus?,
+    private val watchDelay: Duration?,
+    @Suppress("UNUSED_PARAMETER") primaryConstructorMarker: Unit
 ) : EnumerableConfigurationPropertySource {
+
+    constructor(
+        fileLocation: FileLocation,
+        delegateFactory: (String) -> EnumerableConfigurationPropertySource,
+        priority: Priority = Priority.Normal
+    ) :
+            this(fileLocation, delegateFactory, priority, null, null, null, Unit)
+
+    constructor(
+        fileLocation: FileLocation,
+        delegateFactory: (String) -> EnumerableConfigurationPropertySource,
+        priority: Priority = Priority.Normal,
+        watcherCoroutineScope: CoroutineScope,
+        eventBus: LocalEventBus,
+        watchDelay: Duration = Duration.INFINITE
+    ) :
+            this(fileLocation, delegateFactory, priority, watcherCoroutineScope, eventBus, watchDelay, Unit)
 
     private val delegateHolder = AtomicRef<EnumerableConfigurationPropertySource?>(null)
 
     private val delegate get() = delegateHolder.value
 
     init {
-        require(watchDelay > Duration.ZERO)
-        require((watcherCoroutineScope != null && eventBus != null) || (watcherCoroutineScope == null && eventBus == null))
+        require(watchDelay == null || watchDelay > Duration.ZERO)
 
         val initialProperties = tryReadConfiguration()
             ?: throw ConfigurationException("Failed to read configuration: ${fileLocation.path}")
 
-        if (watcherCoroutineScope != null && eventBus != null) {
+        if (watcherCoroutineScope != null && eventBus != null && watchDelay != null) {
             watcherCoroutineScope.launch {
                 var previousPropertiesDefinition = initialProperties
 
