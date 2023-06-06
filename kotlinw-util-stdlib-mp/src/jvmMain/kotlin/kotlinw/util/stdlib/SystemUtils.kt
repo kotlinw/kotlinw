b56f65e7b206f8513b5fe644cdaf052a8005e7dd
Norbert Sándor <sandor.norbert@erinors.com>
2023-06-06 23:27:40 +0200

    improve OS detection

--- kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/SystemUtils.kt
+++ kotlinw-util-stdlib-mp/src/jvmMain/kotlin/kotlinw/util/stdlib/SystemUtils.kt
@@ -1,5 +1,7 @@
 package kotlinw.util.stdlib
 
+import java.io.File
+
 enum class OperatingSystemType {
     Unknown,
     Mac,
@@ -9,12 +11,19 @@ enum class OperatingSystemType {
 }
 
 val operatingSystemType by lazy {
-    val os = System.getProperty("os.name").lowercase()
-    when {
-        os.contains("win") -> OperatingSystemType.Windows
-        os.contains("nix") || os.contains("nux") || os.contains("aix") -> OperatingSystemType.Linux
-        os.contains("mac") -> OperatingSystemType.Mac
-        os.contains("sunos") -> OperatingSystemType.Solaris
-        else -> OperatingSystemType.Unknown
+    val os = System.getProperty("os.name")?.lowercase()
+    if (os != null) {
+        when {
+            os.contains("win") -> OperatingSystemType.Windows
+            os.contains("nix") || os.contains("nux") || os.contains("aix") -> OperatingSystemType.Linux
+            os.contains("mac") -> OperatingSystemType.Mac
+            os.contains("sunos") -> OperatingSystemType.Solaris
+            else -> OperatingSystemType.Unknown
+        }
+    } else {
+        when (File.separatorChar) {
+            '\\' -> OperatingSystemType.Windows
+            else -> OperatingSystemType.Unknown
+        }
     }
 }
