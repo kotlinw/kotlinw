f9b581b2ccd147a437da750db82413ee54957862
Norbert Sándor <sandor.norbert@erinors.com>
2023-08-01 12:23:04 +0200

    Fix build

---
+++ kotlinw-keyvaluestore-platform/src/nativeMain/kotlin/xyz/kotlinw/keyvaluestore/platform/PlatformMutableKeyValueStore.native.kt
@@ -0,0 +1,33 @@
+package xyz.kotlinw.keyvaluestore.platform
+
+import kotlinx.io.bytestring.ByteString
+import xyz.kotlinw.keyvaluestore.api.Key
+import xyz.kotlinw.keyvaluestore.api.MutableKeyValueStore
+
+actual class PlatformMutableKeyValueStore actual constructor(storeId: String) :
+    MutableKeyValueStore.TextValueSupport, MutableKeyValueStore.BinaryValueSupport {
+
+    override suspend fun contains(key: Key): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTextValueOrNull(key: Key): String? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getBinaryValueOrNull(key: Key): ByteString? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun remove(key: Key) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setValue(key: Key, value: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setValue(key: Key, value: ByteString) {
+        TODO("Not yet implemented")
+    }
+}
