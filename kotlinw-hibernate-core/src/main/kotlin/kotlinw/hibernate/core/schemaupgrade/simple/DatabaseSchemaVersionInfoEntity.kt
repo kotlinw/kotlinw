729dd3e732c0da2c21fa975664815e1c7203c94b
Norbert Sándor <sandor.norbert@erinors.com>
2023-08-07 18:00:22 +0200

    Code cleanup

--- kotlinw-hibernate-core/src/main/kotlin/kotlinw/hibernate/core/schemaupgrade/simple/DatabaseSchemaVersionInfoEntity.kt
+++ kotlinw-hibernate-core/src/main/kotlin/kotlinw/hibernate/core/schemaupgrade/simple/DatabaseSchemaVersionInfoEntity.kt
@@ -5,13 +5,14 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import kotlinw.hibernate.core.entity.BaseEntity
+import kotlinw.hibernate.core.entity.pgTextType
 import java.time.Instant
 
 @Entity
 @Table(name = "DatabaseSchemaVersionInfo")
 class DatabaseSchemaVersionInfoEntity(
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = pgTextType)
     var currentSchemaVersion: String,
 
     @Column(nullable = false)
