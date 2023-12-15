548a6705553abf382997591e4fa9e533ad64d63e
Norbert Sándor <sandor.norbert@erinors.com>
2023-12-15 20:03:18 +0100

    refactoring

--- kotlinw-remoting-server-spring/src/main/kotlin/kotlinw/remoting/server/spring/RemotingServerSpringModule.kt
+++
@@ -1,8 +0,0 @@
-package kotlinw.remoting.server.spring
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration(proxyBeanMethods = false)
-@ComponentScan
-class RemotingServerSpringModule
