998b5500bf7e1a81bbf5977f54f8b0208254f8fa
Norbert Sándor <sandor.norbert@erinors.com>
2024-01-06 18:24:03 +0100

    +TODO

--- kotlinw-oauth2-ktor-client/src/commonMain/kotlin/xyz/kotlinw/oauth2/ktor/client/HttpClientUtil.kt
+++ kotlinw-oauth2-ktor-client/src/commonMain/kotlin/xyz/kotlinw/oauth2/ktor/client/HttpClientUtil.kt
@@ -5,5 +5,6 @@ import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerAuthProvider
 import io.ktor.client.plugins.pluginOrNull
 
+// TODO move to more general place
 fun HttpClient.cleanBearerTokens() =
     pluginOrNull(Auth)?.providers?.filterIsInstance<BearerAuthProvider>()?.firstOrNull()?.clearToken()
