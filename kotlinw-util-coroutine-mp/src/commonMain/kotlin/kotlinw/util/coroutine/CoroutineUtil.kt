13ff72e5b05870f9a344a5336e7b30218e2287b6
Norbert Sándor <sandor.norbert@erinors.com>
2024-01-09 12:28:17 +0100

    Refactoring

--- kotlinw-util-coroutine-mp/src/commonMain/kotlin/kotlinw/util/coroutine/CoroutineUtil.kt
+++ kotlinw-util-coroutine-mp/src/commonMain/kotlin/kotlinw/util/coroutine/CoroutineUtil.kt
@@ -1,24 +1,22 @@
 package kotlinw.util.coroutine
 
-import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.job
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 fun CoroutineScope.createNestedScope(additionalCoroutineContext: CoroutineContext = EmptyCoroutineContext): CoroutineScope =
     CoroutineScope(
-        Job(coroutineContext.job) +
-                additionalCoroutineContext +
-                (coroutineContext[CoroutineExceptionHandler] ?: EmptyCoroutineContext)
+        coroutineContext +
+                Job(coroutineContext.job) +
+                additionalCoroutineContext
     )
 
 fun CoroutineScope.createNestedSupervisorScope(additionalCoroutineContext: CoroutineContext = EmptyCoroutineContext): CoroutineScope =
     CoroutineScope(
-        SupervisorJob(coroutineContext.job) +
-                additionalCoroutineContext +
-                (coroutineContext[CoroutineExceptionHandler] ?: EmptyCoroutineContext)
+        coroutineContext +
+                SupervisorJob(coroutineContext.job) +
+                additionalCoroutineContext
     )
