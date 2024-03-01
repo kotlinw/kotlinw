f7fc17b71541a30536ecfb6948a0c63649e667ab
Norbert Sándor <sandor.norbert@erinors.com>
2024-03-01 15:52:49 +0100

    + TODO

--- module/kotlinw-module-webapp-client/src/jsMain/kotlin/xyz/kotlinw/module/webapp/client/NetworkConnectionStatus.kt
+++ module/kotlinw-module-webapp-client/src/jsMain/kotlin/xyz/kotlinw/module/webapp/client/NetworkConnectionStatus.kt
@@ -1,5 +1,6 @@
 package xyz.kotlinw.module.webapp.client
 
+// TODO ez egy belső állapot legyen, kifelé nincs értelme megkülönböztetni az UpdatingStatus-t
 enum class NetworkConnectionStatus(val isConnected: Boolean) {
 
     Connected(true), NotConnected(false), UpdatingStatus(false);
