a1ca17ca83825e8990317e2288b0fc880a8d9f96
Norbert Sándor <sandor.norbert@erinors.com>
2024-05-06 10:25:47 +0200

    Code cleanup

--- kotlinw-hibernate-repository/src/main/kotlin/xyz/kotlinw/hibernate/repository/spi/HibernateBaseEntityIdGenerator.kt
+++ kotlinw-hibernate-repository/src/main/kotlin/xyz/kotlinw/hibernate/repository/spi/HibernateBaseEntityIdGenerator.kt
@@ -2,15 +2,11 @@ package xyz.kotlinw.hibernate.repository.spi
 
 import java.lang.reflect.Member
 import java.util.*
-import org.hibernate.boot.model.relational.Database
-import org.hibernate.boot.model.relational.SqlStringGenerationContext
-import org.hibernate.engine.spi.SharedSessionContractImplementor
 import org.hibernate.generator.EventType
+import org.hibernate.generator.Generator
 import org.hibernate.id.IdentifierGenerator
 import org.hibernate.id.OptimizableGenerator
 import org.hibernate.id.PersistentIdentifierGenerator
-import org.hibernate.id.enhanced.DatabaseStructure
-import org.hibernate.id.enhanced.Optimizer
 import org.hibernate.id.enhanced.SequenceStyleGenerator
 import org.hibernate.id.factory.spi.CustomIdGeneratorCreationContext
 import org.hibernate.service.ServiceRegistry
@@ -26,7 +22,7 @@ class HibernateBaseEntityIdGenerator(
     private val configurationAnnotation: HibernateBaseEntityId,
     annotatedMember: Member,
     private val context: CustomIdGeneratorCreationContext
-) : SequenceStyleGenerator() {
+) : SequenceStyleGenerator(), Generator by sharedGenerator {
 
     override fun configure(type: Type, parameters: Properties, serviceRegistry: ServiceRegistry) {
         if (::sharedGenerator.isInitialized) {
@@ -53,46 +49,7 @@ class HibernateBaseEntityIdGenerator(
         }
     }
 
-    override fun generatesOnUpdate(): Boolean = sharedGenerator.generatesOnUpdate()
-
     override fun generatedOnExecution(): Boolean = sharedGenerator.generatedOnExecution()
 
-    override fun generatedOnExecution(entity: Any?, session: SharedSessionContractImplementor?): Boolean =
-        sharedGenerator.generatedOnExecution(entity, session)
-
-    override fun allowAssignedIdentifiers(): Boolean = sharedGenerator.allowAssignedIdentifiers()
-
-    override fun generatesSometimes(): Boolean = sharedGenerator.generatesSometimes()
-
-    override fun generatesOnInsert(): Boolean = sharedGenerator.generatesOnInsert()
-
     override fun getEventTypes(): EnumSet<EventType> = sharedGenerator.getEventTypes()
-
-    override fun generate(
-        session: SharedSessionContractImplementor?,
-        owner: Any?,
-        currentValue: Any?,
-        eventType: EventType?
-    ): Any = sharedGenerator.generate(session, owner, currentValue, eventType)
-
-    override fun supportsJdbcBatchInserts(): Boolean = sharedGenerator.supportsJdbcBatchInserts()
-
-    override fun generate(session: SharedSessionContractImplementor?, `object`: Any?): Any =
-        sharedGenerator.generate(session, `object`)
-
-    override fun registerExportables(database: Database?) = sharedGenerator.registerExportables(database)
-
-    override fun initialize(context: SqlStringGenerationContext?) = sharedGenerator.initialize(context)
-
-    override fun getOptimizer(): Optimizer = sharedGenerator.getOptimizer()
-
-    override fun supportsBulkInsertionIdentifierGeneration(): Boolean =
-        sharedGenerator.supportsBulkInsertionIdentifierGeneration()
-
-    override fun determineBulkInsertionIdentifierGenerationSelectFragment(context: SqlStringGenerationContext?): String =
-        sharedGenerator.determineBulkInsertionIdentifierGenerationSelectFragment(context)
-
-    override fun getDatabaseStructure(): DatabaseStructure = sharedGenerator.getDatabaseStructure()
-
-    override fun getIdentifierType(): Type = sharedGenerator.getIdentifierType()
 }
