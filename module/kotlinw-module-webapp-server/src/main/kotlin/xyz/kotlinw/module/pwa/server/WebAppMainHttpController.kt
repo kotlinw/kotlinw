edbae9af219a891af89b1a89d834cc10cb0d7387
Norbert Sándor <sandor.norbert@erinors.com>
2024-06-03 00:36:35 +0200

    Experimenting

--- module/kotlinw-module-webapp-server/src/main/kotlin/xyz/kotlinw/module/pwa/server/WebAppMainHttpController.kt
+++ module/kotlinw-module-webapp-server/src/main/kotlin/xyz/kotlinw/module/pwa/server/WebAppMainHttpController.kt
@@ -1,7 +1,6 @@
 package xyz.kotlinw.module.pwa.server
 
 import io.ktor.http.ContentType.Text
-import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
@@ -37,59 +36,70 @@ class WebAppMainHttpController(
 
     private fun setupRouting(
         route: Route,
-        webAppMainHttpController: WebAppMainHttpController
+        webAppMainHttpController: WebAppMainHttpController // TODO ez miért is kell?
     ) {
-        route.route("/") {
-            // TODO további oldalakat is kiszolgálni, ne csak a root-ot
-            // TODO átnézni: https://gist.github.com/hal0gen/5852bd9db240c477f20c
-            val title = "PWA" // TODO paraméterként + i18n
-            val applicationFilePath = if (deploymentMode == Development)
-                "/app.js" // TODO lehetne valami directory is, de eddig nem sikerült úgy bekonfigolni a webapp build-et :\
-            else
-                "/app/pwa/js/app.js" // TODO konfigurálható
+        with(route)
+        {
+            route("/") { // FIXME
+                serveMainHtmlPage(webAppMainHttpController)
+            }
+            route(Regex("admin/.+")) { // FIXME
+                serveMainHtmlPage(webAppMainHttpController)
+            }
+        }
+    }
 
-            get {
-                val initialWebAppClientEnvironmentData = with(call) {
-                    InitialWebAppClientEnvironmentData(
-                        webAppMainHttpController.webAppServerEnvironmentProvider.localeId,
-                        webAppMainHttpController.webAppServerEnvironmentProvider.authenticationStatus
+    private fun Route.serveMainHtmlPage(webAppMainHttpController: WebAppMainHttpController) {
+        // TODO további oldalakat is kiszolgálni, ne csak a root-ot
+        // TODO átnézni: https://gist.github.com/hal0gen/5852bd9db240c477f20c
+        val title = "PWA" // TODO paraméterként + i18n
+        val applicationFilePath = if (deploymentMode == Development)
+            "/app.js" // TODO lehetne valami directory is, de eddig nem sikerült úgy bekonfigolni a webapp build-et :\
+        else
+            "/app/pwa/js/app.js" // TODO konfigurálható
+
+        get {
+            val initialWebAppClientEnvironmentData = with(call) {
+                InitialWebAppClientEnvironmentData(
+                    webAppMainHttpController.webAppServerEnvironmentProvider.localeId,
+                    webAppMainHttpController.webAppServerEnvironmentProvider.authenticationStatus
+                )
+            }
+
+            // TODO html/lang
+            // TODO theme-color
+            // TODO további meta tag-ek
+            // TODO service worker registration in 'load' event: https://web.dev/articles/service-workers-registration
+            // TODO splash
+            // TODO workaround using 'originalFetch': https://youtrack.jetbrains.com/issue/KTOR-539/Ability-to-use-browser-cookie-storage
+
+            val serviceWorkerWebPath = "/app/pwa/js/sw.js" // TODO konfigurálható
+            call.respondText(
+                """
+                    <!doctype html>
+                    <html lang="hu">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta name="theme-color" content="#000000" />
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                            <link rel="manifest" href="/manifest.webmanifest">
+                            <title>$title</title>
+                            <script>
+
+                                window.originalFetch = window.fetch;
+                                window.fetch = function (resource, init) {
+                                    return window.originalFetch(resource, Object.assign({ credentials: 'include' }, init || {}));
+                                };
+                            
+                                if ('serviceWorker' in navigator) {
+                                    navigator.serviceWorker.register('$serviceWorkerWebPath');
+                                }
+                                var $initialWebAppClientEnvironmentJsDataVariableName = `${
+                    serializerService.serialize(
+                        initialWebAppClientEnvironmentData
                     )
-                }
-
-                // TODO html/lang
-                // TODO theme-color
-                // TODO további meta tag-ek
-                // TODO service worker registration in 'load' event: https://web.dev/articles/service-workers-registration
-                // TODO splash
-                // TODO workaround using 'originalFetch': https://youtrack.jetbrains.com/issue/KTOR-539/Ability-to-use-browser-cookie-storage
-
-                val serviceWorkerWebPath = "/app/pwa/js/sw.js" // TODO konfigurálható
-                call.respondText(
-                    """
-                                <!doctype html>
-                                <html lang="hu">
-                                    <head>
-                                        <meta charset="UTF-8">
-                                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                        <meta name="theme-color" content="#000000" />
-                                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                                        <link rel="manifest" href="/manifest.webmanifest">
-                                        <title>$title</title>
-                                        <script>
-    
-                                            window.originalFetch = window.fetch;
-                                            window.fetch = function (resource, init) {
-                                                return window.originalFetch(resource, Object.assign({ credentials: 'include' }, init || {}));
-                                            };
-                                        
-                                            if ('serviceWorker' in navigator) {
-                                                navigator.serviceWorker.register('$serviceWorkerWebPath');
-                                            }
-                                            var $initialWebAppClientEnvironmentJsDataVariableName = `${
-                        serializerService.serialize(
-                            initialWebAppClientEnvironmentData
-                        )
-                    }`;
+                }`;
                                         </script>
                                     </head>
                                     <body>
@@ -98,9 +108,8 @@ class WebAppMainHttpController(
                                     </body>
                                 </html>
                             """.trimIndent(),
-                    Text.Html
-                )
-            }
+                Text.Html
+            )
         }
     }
 }
