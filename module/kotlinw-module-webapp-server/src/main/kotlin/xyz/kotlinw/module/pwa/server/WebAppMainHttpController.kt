40e082c76f287fc81bf93fd653700d4abd784ab2
Norbert Sándor <sandor.norbert@erinors.com>
2024-06-10 21:22:13 +0200

    + webfont

--- module/kotlinw-module-webapp-server/src/main/kotlin/xyz/kotlinw/module/pwa/server/WebAppMainHttpController.kt
+++ module/kotlinw-module-webapp-server/src/main/kotlin/xyz/kotlinw/module/pwa/server/WebAppMainHttpController.kt
@@ -68,6 +68,7 @@ class WebAppMainHttpController(
             // TODO splash
             // TODO workaround using 'originalFetch': https://youtrack.jetbrains.com/issue/KTOR-539/Ability-to-use-browser-cookie-storage
 
+            // TODO roboto font beégetve
             val serviceWorkerWebPath = "/app/pwa/js/sw.js" // TODO konfigurálható
             call.respondText(
                 """
@@ -95,7 +96,10 @@ class WebAppMainHttpController(
                         initialWebAppClientEnvironmentData
                     )
                 }`;
-                                        </script>
+                                    </script>
+                                    <style>
+                                    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+                                    </style>                                        
                                     </head>
                                     <body>
                                         <div id="root"></div>
